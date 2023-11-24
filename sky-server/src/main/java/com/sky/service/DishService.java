19c7bc396f474a6d1c496a005465d7f1bf1aa072
Mio <1181072169@qq.com>
2023-11-24 20:28:32 +0800

    删除菜品完成

--- sky-server/src/main/java/com/sky/service/DishService.java
+++ sky-server/src/main/java/com/sky/service/DishService.java
@@ -1,8 +1,12 @@
 package com.sky.service;
 
 import com.sky.dto.DishDTO;
+import com.sky.dto.DishPageQueryDTO;
+import com.sky.result.PageResult;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 public interface DishService {
 
@@ -11,4 +15,15 @@ public interface DishService {
     * 新增菜品和口味
     * */
     public void saveWithFlavor(DishDTO dishDTO);
+
+    /*
+    * 菜品分页查询
+    * */
+    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
+
+
+    /*
+    * 菜品批量删除
+    * */
+    void deleteBatch(List<Long> ids);
 }
