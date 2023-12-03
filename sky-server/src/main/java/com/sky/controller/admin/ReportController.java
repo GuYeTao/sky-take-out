5a0e93942fc391ecf2f1d02eee6279292aeb09f5
Mio <1181072169@qq.com>
2023-12-03 21:01:09 +0800

    报表导出完成

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDate;
 
 @RestController
@@ -68,5 +69,11 @@ public class ReportController {
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
         log.info("销量统计：{}-{}",begin, end);
         return Result.success(reportService.getTop10(begin, end));
+    }
+
+    @GetMapping("/export")
+    @ApiOperation("导出报表")
+    public void export(HttpServletResponse response){
+        reportService.exportBusinessData(response);
     }
 }
